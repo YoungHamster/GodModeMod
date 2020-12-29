47aac5627bf4121000911c898b01ba79c0500498
Viktor <36134532+YoungHamster@users.noreply.github.com>
2020-12-29 21:31:35 +0400

    Finished main features

--- main/java/com/viktor/godmodemod/RayTraceStuff.java
+++ main/java/com/viktor/godmodemod/RayTraceStuff.java
@@ -1,22 +1,49 @@
 package com.viktor.godmodemod;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
+import java.util.List;
+import java.util.Optional;
+
 public class RayTraceStuff {
-    public static BlockRayTraceResult rayTraceBlockFromPlayer(World world, PlayerEntity player, double range){
+    private static Vector3d getStartVec(PlayerEntity player){
         double startX = player.getPositionVec().x;
-        double startY = player.getPositionVec().y + player.getHeight();
+        double startY = player.getPositionVec().y + player.getEyeHeight();
         double startZ = player.getPositionVec().z;
-        Vector3d startVec = new Vector3d(startX, startY, startZ);
-        double endX = startX + player.getLookVec().x*range;
-        double endY = startY + player.getLookVec().y*range;
-        double endZ = startZ + player.getLookVec().z*range;
-        Vector3d endVec = new Vector3d(endX, endY, endZ);
-        return world.rayTraceBlocks(new RayTraceContext(startVec, endVec, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.ANY, player));
+        return new Vector3d(startX, startY, startZ);
+    }
+
+    private static Vector3d getEndVec(PlayerEntity player, Vector3d startVec, double range){
+        double endX = startVec.x + player.getLookVec().x*range;
+        double endY = startVec.y + player.getLookVec().y*range;
+        double endZ = startVec.z + player.getLookVec().z*range;
+        return new Vector3d(endX, endY, endZ);
+    }
+
+    public static BlockRayTraceResult rayTraceBlockFromPlayer(World world, PlayerEntity player, double range){
+        Vector3d startVec = getStartVec(player);
+        Vector3d endVec = getEndVec(player, startVec, range);
+        return world.rayTraceBlocks(new RayTraceContext(startVec, endVec, RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.ANY, null));
+    }
+
+    public static Entity rayTraceEntityFromPlayer(PlayerEntity player, World world, double range){
+        Vector3d startVec = getStartVec(player);
+        Vector3d endVec = getEndVec(player, startVec, range);
+        AxisAlignedBB entitiesBB = new AxisAlignedBB(
+                player.getPosX() - range, player.getPosY() - range, player.getPosZ() - range,
+                player.getPosX() + range, player.getPosY() + range, player.getPosZ() + range);
+        List<Entity> entities = world.getEntitiesInAABBexcluding(player, entitiesBB, null);
+        for(Entity entity: entities){
+            Optional<Vector3d> collision = entity.getBoundingBox().rayTrace(startVec, endVec);
+            if(collision.isPresent())
+                return entity;
+        }
+        return null;
     }
 }
--- main/java/com/viktor/godmodemod/StickHandler.java
+++ main/java/com/viktor/godmodemod/StickHandler.java
@@ -1,44 +1,36 @@
 package com.viktor.godmodemod;
 
-import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.CowEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.World;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraftforge.event.entity.player.AnvilRepairEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.thread.SidedThreadGroups;
-import org.lwjgl.system.CallbackI;
-import org.spongepowered.asm.launch.Phases;
-
-import java.util.List;
 
 public class StickHandler {
 
     public static boolean playeralreadyinvincible = false;
 
-    // Event is called 2 times every time player right clicks so i use this counter to execute it only 1 time
-    public static int onStickUseCounter = 0;
     private static final RayTraceStuff rayTraceStuff = new RayTraceStuff();
 
     @SubscribeEvent
     public void onStickUse(PlayerInteractEvent.RightClickItem event) {
-        onStickUseCounter += 1;
-        if(onStickUseCounter % 2 == 1) return;
+        if(Thread.currentThread().getThreadGroup() != SidedThreadGroups.SERVER) return;
         if(!event.getItemStack().hasTag()) return;
         if (event.getItemStack().getTag().toString().contains("killing stick")) {
-            event.getPlayer().getLookVec();
+            Entity entity = rayTraceStuff.rayTraceEntityFromPlayer(event.getPlayer(), event.getWorld(), 50);
+            if(entity != null) {
+                event.getPlayer().sendMessage(new StringTextComponent("Woosh! It's gone"),
+                        event.getPlayer().getUniqueID());
+                entity.remove();
+            }
         }
         if (event.getItemStack().getTag().toString().contains("spawning stick")) {
             BlockRayTraceResult result = rayTraceStuff.rayTraceBlockFromPlayer(event.getWorld(), event.getPlayer(), 20);
-            CowEntity entity = new CowEntity(EntityType.COW, event.getWorld());
             double dx = 0;
             double dy = 0;
             double dz = 0;
@@ -48,21 +40,49 @@ public class StickHandler {
                     break;
                 case DOWN: dy = -1;
                     break;
-                case NORTH: dx = -1;
+                case NORTH: dz = -1;
                     break;
-                case SOUTH: dx = 1;
+                case SOUTH: dz = 2;
                     break;
-                case WEST: dz = -1;
+                case WEST: dx = -1;
                     break;
-                case EAST: dz = 1;
+                case EAST: dx = 2;
                     break;
             }
+            CowEntity entity = new CowEntity(EntityType.COW, event.getWorld());
             entity.setPosition(result.getPos().getX() + dx, result.getPos().getY() + dy, result.getPos().getZ() + dz);
             event.getWorld().addEntity(entity);
+            event.getPlayer().sendMessage(new StringTextComponent("You spawned a cow"),
+                    event.getPlayer().getUniqueID());
         }
         if (event.getItemStack().getTag().toString().contains("invincibility stick")) {
             playeralreadyinvincible = !playeralreadyinvincible;
             event.getPlayer().setInvulnerable(playeralreadyinvincible);
+            if(playeralreadyinvincible){
+                event.getPlayer().sendMessage(new StringTextComponent("You've made yourself invincible"),
+                        event.getPlayer().getUniqueID());
+            }
+            else{
+                event.getPlayer().sendMessage(new StringTextComponent("You've made yourself not invincible anymore"),
+                        event.getPlayer().getUniqueID());
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void tellPlayerTheyreAGodNow(AnvilRepairEvent event){
+        if(Thread.currentThread().getThreadGroup() != SidedThreadGroups.SERVER) return;
+        if(event.getItemResult().getTag().toString().contains("killing stick")){
+            event.getPlayer().sendMessage(new StringTextComponent("So you're now a god of death"),
+                    event.getPlayer().getUniqueID());
+        }
+        if(event.getItemResult().getTag().toString().contains("spawning stick")){
+            event.getPlayer().sendMessage(new StringTextComponent("So you're now a god of life"),
+                    event.getPlayer().getUniqueID());
+        }
+        if(event.getItemResult().getTag().toString().contains("invincibility stick")){
+            event.getPlayer().sendMessage(new StringTextComponent("So you're now a god"),
+                    event.getPlayer().getUniqueID());
         }
     }
 }
